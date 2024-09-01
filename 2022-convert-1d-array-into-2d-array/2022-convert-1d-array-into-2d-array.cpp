0b0227cb00e32efd523b97746e68b9fad20b2aa2
Anish <112772218+AnishMane@users.noreply.github.com>
2024-09-01 08:41:27 +0530

    Time: 81 ms (61.08%), Space: 88.1 MB (55.59%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -2,40 +2,14 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         int size = original.size();
-        if(m*n == 1 && size!=1){
+        if(size!= m*n){
             return {};
         }
-        if(size%(m*n) != 0 ){
-            return {};
-        }
-        // else if(n==1){
-        //     if(m!= original.size()){
-        //         return {};
-        //     }
-        // }else if(m == 1){
-        //     if(n!= original.size()){
-        //         return {};
-        //     }
-        // }
         
-        vector<vector<int>> result;
-        vector<int> temp;
-        int count = 0;
-        int mcount = 0;
-        for(int i=0;i<original.size();i++){
-            if(mcount>m){
-                return {};
-            }
-            temp.push_back(original[i]);
-            count++;
-            if(count == n){
-                result.push_back(temp);
-                count = 0;
-                mcount++;
-                temp.clear();
-            }
+        vector<vector<int>> result(m,vector<int>(n));
+        for(int i=0;i<size;i++){
+            result[i/n][i%n] = original[i];
         }
         return result;
-        
     }
 };
