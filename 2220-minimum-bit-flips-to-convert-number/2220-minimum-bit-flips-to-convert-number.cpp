549af20bea26cf31a1955a9c339adba9c3e40e02
Anish <112772218+AnishMane@users.noreply.github.com>
2024-09-11 13:17:17 +0530

    Time: 3 ms (41.47%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    string convertDtoB(int n){
-        string res = "";
-        while(n!=1){
-            if(n%2 == 1){
-                res+='1';
-            }
-            else{
-                res+='0';
-            }
-            n = n/2;
-        }
-        res+= '1';
-        reverse(res.begin(),res.end());
-        return res;
-    }
-    
     int minBitFlips(int start, int goal) {
-        int calc = start ^ goal;
-        if(calc == 0){
-            return 0;
+        int count = 0;
+        int calc = start^goal;
+        
+        while(calc!=0){
+            count += calc & 1;
+            calc >>= 1;
         }
-        string cal = convertDtoB(calc);
-        int result = 0;
-        for(int i=0;i<cal.size();++i){
-            if(cal[i] == '1'){
-                result++;
-            }
-        }
-        return result;
+        return count;
     }
 };
