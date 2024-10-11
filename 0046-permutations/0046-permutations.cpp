1c8143cb75cd0a1d9f303eb81c838acdaccf402f
Anish <112772218+AnishMane@users.noreply.github.com>
2024-10-11 10:15:51 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (46.59%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    void permutations(int i, vector<int>&nums,vector<vector<int>>& result, vector<int>&res, unordered_map<int,bool>&hashMap){
+    void permutations(int i, vector<int>&nums,vector<vector<int>>& result, vector<int>&res, vector<bool>& freq){
         if(i==nums.size()){
             result.push_back(res);
             return;
         }
         for(int j=0;j<nums.size();j++){
-            if(hashMap[nums[j]])
+            if(freq[j])
                 continue;
             res.push_back(nums[j]);
-            hashMap[nums[j]] = true;
-            permutations(i+1,nums,result,res,hashMap);
+            freq[j] = true;
+            permutations(i+1,nums,result,res,freq);
             res.pop_back();
-            hashMap[nums[j]] = false;
+            freq[j] = false;
 
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_map<int,bool> hashMap;
-        for(int num:nums){
-            hashMap[num] = false;
-        }
+        vector<bool> freq(nums.size(),false);
         vector<vector<int>> result;
         vector<int> res;
-        permutations(0,nums,result,res,hashMap);
+        permutations(0,nums,result,res,freq);
         return result;
     }
 };
