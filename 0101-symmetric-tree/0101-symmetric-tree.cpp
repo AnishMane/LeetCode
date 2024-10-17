11051794c03faa5d498e0831a3c69ac72c310ff8
Anish <112772218+AnishMane@users.noreply.github.com>
2024-10-17 21:38:26 +0530

    Time: 0 ms (100.00%), Space: 17.5 MB (67.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,68 +11,16 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root){
-        queue<TreeNode*> q;
-        if(!root) return false;
-        q.push(root);
-        q.pop();
-        if(root->left && root->right){
-            q.push(root->left);
-            q.push(root->right);
-        }
-        else if(!root->left && !root->right){
-            return true;
-        }
-        else{
-            return false;
-        }
-        while(!q.empty()){
-            vector<int> temp1;
-            vector<int> temp2;
-            int qLen = q.size();
-            if(qLen%2 != 0) return false;
-            for(int i=0;i<qLen/2;i++){
-                TreeNode* t = q.front();
-                if(!t){
-                    temp1.push_back(INT_MIN);
-                    q.pop();
-                    continue;
-                } 
-                else
-                    temp1.push_back(t->val);
-                q.pop();
-                if(t->left)
-                    q.push(t->left);
-                else
-                    q.push(NULL);
-                if(t->right)
-                    q.push(t->right);
-                else
-                    q.push(NULL);
-            }
-            for(int i=qLen/2;i<qLen;i++){
-                TreeNode* t = q.front();
-                if(!t){
-                     temp2.push_back(INT_MIN);
-                    q.pop();
-                    continue;
-                }
-                else
-                    temp2.push_back(t->val);
-                q.pop();
-                if(t->left)
-                    q.push(t->left);
-                else
-                    q.push(NULL);
-                if(t->right)
-                    q.push(t->right);
-                else
-                    q.push(NULL);
-            }
-            reverse(temp2.begin(),temp2.end());
-            if(temp1 != temp2) return false;
-        }
-        return true;
-
+    
+    bool checkSymmetry(TreeNode* left, TreeNode* right){
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        return left->val==right->val && checkSymmetry(left->left, right->right) && checkSymmetry(left->right, right->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        if(!root->left && !root->right) return true;
+        if(!root->left || !root->right) return false;
+        return checkSymmetry(root->left,root->right);
     }
 };
