1344bcd3db8890a6f8fc5698f1c7df4efe3b9867
Anish <112772218+AnishMane@users.noreply.github.com>
2024-11-17 17:30:39 +0530

    Time: 3 ms (55.92%), Space: 27.9 MB (54.34%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,44 +1,37 @@
 class Solution {
 private:
-    int result = 0;
+    int count;
 public:
     
-    void bfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis, int delRow[], int delCol[]){
-        queue<pair<int,int>>q;
-        vis[i][j] = 1;
-        q.push({i,j});
-        int count = 1;
-        
+    void dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis, int delRow[], int delCol[]){
+        vis[row][col] = 1;
         int n = grid.size();
         int m = grid[0].size();
-        
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+        for(int i=0;i<4;i++){
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
             
-            for(int s=0;s<4;s++){
-                int nrow = row + delRow[s];
-                int ncol = col + delCol[s];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                    count++;
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                count++;
+                dfs(nrow,ncol,grid,vis,delRow,delCol);
             }
         }
-        result = max(result,count);
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(), 0));
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
         int delRow[] = {0,+1,0,-1};
         int delCol[] = {-1,0,+1,0};
+        
+        int result = 0;
+        
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(!vis[i][j] && grid[i][j] == 1)
-                    bfs(i,j,grid,vis,delRow,delCol);
+                if(!vis[i][j] && grid[i][j] == 1){
+                    count = 1;
+                    dfs(i,j,grid,vis,delRow,delCol);
+                    result = max(result,count);
+                }
             }
         }
         return result;
