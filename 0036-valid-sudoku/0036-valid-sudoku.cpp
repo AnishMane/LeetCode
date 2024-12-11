3083009a6048c4faf34a78c4eed36cfe093c972a
Anish <112772218+AnishMane@users.noreply.github.com>
2024-12-11 10:36:18 +0530

    Time: 7 ms (38.52%), Space: 24 MB (61.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;++i){
+        for(int i=0;i<board.size();i++){
             unordered_set<char> rowSet;
             unordered_set<char> colSet;
-            for(int j=0;j<9;++j){
-                if(board[i][j] != '.'){
-                    if(rowSet.count(board[i][j])){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]!='.'){
+                    if(rowSet.count(board[i][j]))
                         return false;
-                    }
                     rowSet.insert(board[i][j]);
                 }
                 if(board[j][i] != '.'){
-                    if(colSet.count(board[j][i])){
+                    if(colSet.count(board[j][i]))
                         return false;
-                    }
                     colSet.insert(board[j][i]);
                 }
             }
@@ -22,17 +20,14 @@ public:
         
         for(int i=0;i<9;i+=3){
             for(int j=0;j<9;j+=3){
-                unordered_set<char> boxSet;
-                for(int a=0;a<3;++a){
-                    for(int b=0;b<3;++b){
-                        if(board[i+a][j+b] != '.')
-                        {
-                            if(boxSet.count(board[i+a][j+b])){
+                unordered_set<char> blockSet;
+                for(int a=0;a<3;a++){
+                    for(int b=0;b<3;b++){
+                        if(board[i+a][j+b]!='.'){
+                            if(blockSet.count(board[i+a][j+b]))
                                 return false;
-                            }
-                            boxSet.insert(board[i+a][j+b]);
+                            blockSet.insert(board[i+a][j+b]);
                         }
-                        
                     }
                 }
             }
