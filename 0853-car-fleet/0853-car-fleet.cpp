e82506b2861ac7067af7d93db1e0142bc7626b67
Anish <112772218+AnishMane@users.noreply.github.com>
2024-12-26 19:20:34 +0530

    Time: 39 ms (67.78%), Space: 82.3 MB (78.60%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int,int>> ps;
-        stack<double> s;
-        for(int i=0;i<position.size();++i){
-            ps.push_back({position[i], speed[i]});
+        vector<pair<int,int>> sorted;
+        stack<double> st;
+        for(int i=0;i<position.size();i++){
+            sorted.push_back({position[i],speed[i]});
         }
-        sort(ps.begin(),ps.end());
+        sort(sorted.begin(),sorted.end());
         
-        for(int i=ps.size()-1;i>=0;--i){
-            double temp = (double(target - ps[i].first)/double(ps[i].second));
-            if(s.empty()){
-                s.push(temp);
-            }
-            else{
-                if(s.top() >= temp){
-                    continue;
-                }else{
-                    s.push(temp);
+        for(int i=sorted.size()-1;i>=0;--i){
+            double timeTaken = double(target-sorted[i].first)/double(sorted[i].second);
+            
+            if(st.empty()){
+                st.push(timeTaken);
+            }else{
+                if(st.top() < timeTaken){
+                    st.push(timeTaken);
                 }
             }
         }
-        return s.size();
+        return st.size();
     }
 };
