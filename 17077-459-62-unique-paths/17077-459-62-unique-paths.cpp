930f9714260709718e9d60c1ab7d1835b6a01970
Anish <112772218+AnishMane@users.noreply.github.com>
2025-01-11 22:28:24 +0530

    Time: 0 ms, Memory: 9.4 MB - LeetHub

--- 17077-459-62-unique-paths/17077-459-62-unique-paths.cpp
+++ 17077-459-62-unique-paths/17077-459-62-unique-paths.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int recursion(int curX, int curY,int m, int n, int total, vector<vector<int>> &dp){
-        if(curX>=m || curY >=n)
+    int recursion(int curX, int curY, vector<vector<int>> &dp){
+        if(curX ==0 && curY == 0)
+            return 1;
+        
+        if(curX <0 || curY <0)
             return 0;
+        
         if(dp[curX][curY] != -1){
             return dp[curX][curY];
         }
-        if(curX == m-1 && curY == n-1){
-            return total+1;
-        }
 
-        int sumX = recursion(curX+1,curY,m,n,total,dp);
-        int sumY = recursion(curX,curY+1,m,n,total,dp);
+        int sumX = recursion(curX-1,curY,dp);
+        int sumY = recursion(curX,curY-1,dp);
         dp[curX][curY] = sumX + sumY;
         return dp[curX][curY];
     }
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return recursion(0,0,m,n,0,dp);
+        return recursion(m-1,n-1,dp);
     }
 };
