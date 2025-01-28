22ba19e84361c3e383ed43df6163f48743585ff0
Anish <112772218+AnishMane@users.noreply.github.com>
2025-01-28 22:07:25 +0530

    Time: 19 ms, Memory: 28 MB - LeetHub

--- 9611-192-518-coin-change-ii/9611-192-518-coin-change-ii.cpp
+++ 9611-192-518-coin-change-ii/9611-192-518-coin-change-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    
-    int recursion(int ind, int target, vector<int> &coins, vector<vector<int>> &dp){
-        if(ind==0){
-            if(target%coins[0]==0) return 1;
-            return 0;
-        }
-        if(dp[ind][target]!= -1) return dp[ind][target];
-        
-        int nottake = 0 + recursion(ind-1,target,coins,dp);
-        int take = 0;
-        if(target>=coins[ind]) take = recursion(ind,target-coins[ind],coins,dp);
-        
-        return dp[ind][target] = take + nottake;
-    }
-    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return recursion(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int k=0;k<=amount;k++){
+            if(k%coins[0]==0) dp[0][k] = 1;
+            else dp[0][k] = 0;
+        }
+        
+        for(int ind=1;ind<n;++ind){
+            for(int target=0;target<=amount;target++){
+                long nottake = dp[ind-1][target];
+                long take = 0;
+                if(target>=coins[ind]) take = dp[ind][target-coins[ind]];
+
+                dp[ind][target] = take + nottake;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
