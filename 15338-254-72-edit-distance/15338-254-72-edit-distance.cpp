ed148218e905b5e269d08f4c821f94dd81245bfb
Anish <112772218+AnishMane@users.noreply.github.com>
2025-02-03 10:07:42 +0530

    Time: 3 ms, Memory: 10.4 MB - LeetHub

--- 15338-254-72-edit-distance/15338-254-72-edit-distance.cpp
+++ 15338-254-72-edit-distance/15338-254-72-edit-distance.cpp
@@ -3,23 +3,24 @@ public:
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        for(int i=0;i<=m;i++) dp[i][0] = i;
-        for(int j=1;j<=n;j++) dp[0][j] = j;
+        vector<int> prev(n+1,0), cur(n+1,0);
+        for(int j=0;j<=n;j++) prev[j] = j;
         
         for(int i=1;i<=m;i++){
+            cur[0] = i;
             for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                if(word1[i-1]==word2[j-1]) cur[j] = 0 + prev[j-1];
                 else{
-                    int insert = 1 + dp[i][j-1];
-                    int del = 1 + dp[i-1][j];
-                    int replace = 1 + dp[i-1][j-1];
+                    int insert = 1 + cur[j-1];
+                    int del = 1 + prev[j];
+                    int replace = 1 + prev[j-1];
 
-                    dp[i][j] = min(insert,min(del,replace));
+                    cur[j] = min(insert,min(del,replace));
                 }
             }
+            prev = cur;
         }
         
-        return dp[m][n];
+        return prev[n];
     }
 };
