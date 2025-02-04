1b0a0309fafae1fa3031ca3f07f68040be6d12b3
Anish <112772218+AnishMane@users.noreply.github.com>
2025-02-04 22:05:32 +0530

    Time: 7 ms, Memory: 18.4 MB - LeetHub

--- 7628-213-188-best-time-to-buy-and-sell-stock-iv/7628-213-188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 7628-213-188-best-time-to-buy-and-sell-stock-iv/7628-213-188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,26 +3,30 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        vector<vector<int>> ahead(2,vector<int>(k+1,0));
+        vector<vector<int>> cur(2,vector<int>(k+1,0));
+
         
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=k;cap++){
                     if(buy==1){
-                        int nottake = dp[ind+1][1][cap];
-                        int take = -prices[ind] + dp[ind+1][0][cap];
+                        int nottake = ahead[1][cap];
+                        int take = -prices[ind] + ahead[0][cap];
 
-                        dp[ind][buy][cap] = max(nottake,take);
+                        cur[buy][cap] = max(nottake,take);
                     }
                     else{
-                        int nottake = dp[ind+1][0][cap];
-                        int take = prices[ind] + dp[ind+1][1][cap-1];
+                        int nottake = ahead[0][cap];
+                        int take = prices[ind] + ahead[1][cap-1];
 
-                        dp[ind][buy][cap] = max(nottake,take);
+                        cur[buy][cap] = max(nottake,take);
                     }
                 }
+                ahead=cur;
             }
         }
         
-        return dp[0][1][k];
+        return ahead[1][k];
     }
 };
