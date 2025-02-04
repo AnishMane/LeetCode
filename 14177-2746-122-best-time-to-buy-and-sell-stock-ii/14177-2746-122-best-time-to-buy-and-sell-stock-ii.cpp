97195b2ed82f5b947f05e22cb4e0553b5f4d7390
Anish <112772218+AnishMane@users.noreply.github.com>
2025-02-04 10:49:20 +0530

    Time: 9 ms, Memory: 19.6 MB - LeetHub

--- 14177-2746-122-best-time-to-buy-and-sell-stock-ii/14177-2746-122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 14177-2746-122-best-time-to-buy-and-sell-stock-ii/14177-2746-122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-
-    int recursion(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp){
-        if(ind==prices.size()) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-
-        if(buy==1){
-            int nottake = recursion(ind+1,1,prices,dp);
-            int take = recursion(ind+1,0,prices,dp) - prices[ind];
-            return dp[ind][buy] = max(nottake,take);
-        }
-        else{
-            int nottake = recursion(ind+1,0,prices,dp);
-            int take = recursion(ind+1,1,prices,dp) + prices[ind];
-
-            return dp[ind][buy] = max(nottake,take);
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>> dp(n+1,vector<int>(2,0));
