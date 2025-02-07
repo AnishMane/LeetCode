e64d98be372f31968d859a3c5980bfe5cf9f2e1b
Anish <112772218+AnishMane@users.noreply.github.com>
2025-02-07 11:47:26 +0530

    Time: 274 ms, Memory: 296.2 MB - LeetHub

--- 21491-469-300-longest-increasing-subsequence/21491-469-300-longest-increasing-subsequence.cpp
+++ 21491-469-300-longest-increasing-subsequence/21491-469-300-longest-increasing-subsequence.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-
-    int recursion(int ind, int prev, vector<int> &nums, vector<vector<int>> &dp){
-        int n = nums.size();
-        if(ind==n) return 0;
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-
-        int nottake = recursion(ind+1,prev,nums,dp);
-        int take = 0;
-        if(prev==-1 || nums[prev]<nums[ind]) take = 1 + recursion(ind+1,ind,nums,dp);
-
-        return dp[ind][prev+1] = max(nottake,take);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         
-        return recursion(0,-1,nums,dp);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int nottake = dp[ind+1][prev+1];
+                int take = 0;
+                if(prev==-1 || nums[prev]<nums[ind]) take = 1 + dp[ind+1][ind+1];
+
+                dp[ind][prev+1] = max(nottake,take);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
