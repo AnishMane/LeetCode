6d00f8ae7d9ff9e5f772db65178a0267c3e61291
Anish <112772218+AnishMane@users.noreply.github.com>
2025-04-05 21:00:22 +0530

    Time: 3 ms, Memory: 13 MB - LeetHub

--- 15497-264-72-edit-distance/15497-264-72-edit-distance.cpp
+++ 15497-264-72-edit-distance/15497-264-72-edit-distance.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-
-    int recursion(int i, int j, string word1, string word2, vector<vector<int>>&dp){
-        if(j==0) return i;
-        if(i==0) return j;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(word1[i-1] == word2[j-1]) return 0 + recursion(i-1,j-1,word1,word2,dp);
-        int insert = 1 + recursion(i,j-1,word1,word2,dp);
-        int del = 1 + recursion(i-1,j,word1,word2,dp);
-        int replace = 1 + recursion(i-1,j-1,word1,word2,dp);
-
-        return dp[i][j] = min(insert,min(del,replace));
-    }
-
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
         
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return recursion(m,n,word1,word2,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++){
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                else{
+                    int insert = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = min(insert,min(del,replace));
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
