630c73efaef181556649313c07f29043d1d20425
Anish <112772218+AnishMane@users.noreply.github.com>
2025-04-07 11:25:12 +0530

    Time: 0 ms, Memory: 20.2 MB - LeetHub

--- 15391-643-45-jump-game-ii/15391-643-45-jump-game-ii.cpp
+++ 15391-643-45-jump-game-ii/15391-643-45-jump-game-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    
-    int recursion(int i, vector<int>&nums, vector<int>&dp){
-        if(i>=nums.size()-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        
-        int mini = 1e9;
-        
-        for(int j=1;j<=nums[i];j++){
-            mini = min(mini,1 + recursion(i+j,nums,dp));
-        }
-        return dp[i] = mini;
-    }
-    
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return recursion(0,nums,dp);
+        int jumps = 0;
+        int l=0, r=0;
+        
+        while(r<n-1){
+            int far =0;
+            for(int i=l;i<=r;i++){
+                far = max(far,i+nums[i]);
+            }
+            l = r+1;
+            r = far;
+            jumps++;
+        }
+        
+        return jumps;
     }
 };
