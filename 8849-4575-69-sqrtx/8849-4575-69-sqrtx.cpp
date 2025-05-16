f0ef33a00c92e3ce4b91ec35ae354210e802c68b
Anish <112772218+AnishMane@users.noreply.github.com>
2025-05-16 22:12:00 +0530

    Time: 0 ms, Memory: 8.6 MB - LeetHub

---
+++ 8849-4575-69-sqrtx/8849-4575-69-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1) return 1;
+        int l = 0,r=x;
+        int res = 0;
+        while(l<=r){
+            int m = l + (r-l)/2;
+            
+
+            if((long long)m*m>x){ 
+                r = m-1;
+            }
+            else if((long long)m*m < x){
+                l = m+1;
+                res = m;
+            }
+            else{
+                return m;
+            }
+        }
+        return res;
+    }
+};
