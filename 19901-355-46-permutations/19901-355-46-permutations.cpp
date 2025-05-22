db1b33138eeb5411e9abfbaaa047c23984c35895
Anish <112772218+AnishMane@users.noreply.github.com>
2025-05-22 11:02:46 +0530

    Time: 0 ms, Memory: 11.3 MB - LeetHub

--- 19901-355-46-permutations/19901-355-46-permutations.cpp
+++ 19901-355-46-permutations/19901-355-46-permutations.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.empty()) return {{}};
-
-        vector<int> tmp = vector<int>(nums.begin()+1,nums.end());
-        vector<vector<int>> perms = permute(tmp);
-        vector<vector<int>> res;
-
-        for(auto& p:perms){
-            for(int i=0;i<=p.size();i++){
-                vector<int> copy = p;
-                copy.insert(copy.begin()+i,nums[0]);
-                res.push_back(copy);
+        vector<vector<int>> perms = {{}};
+        
+        for(int n:nums){
+            vector<vector<int>> new_perms;
+            for(auto& p:perms){
+                for(int i=0;i<=p.size();i++){
+                    vector<int> copy = p;
+                    copy.insert(copy.begin()+i,n);
+                    new_perms.push_back(copy);
+                }
             }
+            perms = new_perms;
         }
-        return res;
+        return perms;
     }
 };
